888cc5f9ac9c95d22ec9f86ea64f074d5be38d70
lee <lee@lee.com>
2019-05-14 14:47:31 +0800

    -add pcl

--- UnitTest/test_cpp.cpp
+++ UnitTest/test_cpp.cpp
@@ -4,6 +4,8 @@
 #include <filesystem>
 #include <tuple>
 #include <iostream>
+#include <algorithm>
+#include <random>
 
 TEST(cpp, filesytem)
 {
@@ -14,4 +16,12 @@ TEST(cpp, tuple)
 {
     auto t = std::make_tuple(1, 2, 3);
     auto[a, b, c] = t;
+}
+
+TEST(cpp, random)
+{
+    std::vector vec = { 0,1,2,3,4,5,6 };
+    std::random_device rd;
+    std::mt19937 rand(rd());
+    std::shuffle(vec.begin(), vec.end(), rand);
 }
